0aa5a54bfd967e4b26f1330100f44b8f794961b0
Sanjay Nair <70877020+sanjaynair16@users.noreply.github.com>
2021-07-29 15:16:42 +0800

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -9,13 +9,13 @@ df = df[df['review']>5]
 df['District']=df['address'].apply(lambda x: x.split(',')[-1])
 
 # Create new column for 'Like%'
-df['Like%'] = df'likes']/(df['dislikes']+df['likes'])*100
+df['Like%'] = df['likes']/(df['dislikes']+df['likes'])*100
 
 # Change invalid input in 'District' to correct location 
 print(df[df['District']=='2及15號舖'].index)
 df.loc[68, 'District'] = 'Western District'
 
-# Update restuarnt provide cusisine more than one location to 'fusion'
+# Update restaurant which provide more than one cuisine to 'fusion'
 df_cuisine = df.groupby('cuisine').mean().sort_values('Popularity',ascending = False)
 df_cuisine
 df_cuisine = df_cuisine.rename(index ={'Shanghai':'Fusion','Taiwan':'Fusion','Brazilian':'Fusion','French':'Fusion','Western':'Fusion','Taiwanese Drink':'Fusion','International':'Fusion'})
